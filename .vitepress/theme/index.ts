d8c313e5b259a77aade569ca72b499f7fd17ad6a
OksmaWarez <75636222+OksmaWarez@users.noreply.github.com>
2026-08-04 16:57:14 +0300

    revert bs

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -7,42 +7,8 @@ import './style.css'
 export default {
   extends: DefaultTheme,
   Layout: () => {
-    const videoSrc = 'https://icloud.betaxp.tk/alien.mp4'
-
     return h(DefaultTheme.Layout, null, {
-      'not-found': () =>
-        h('div', { class: 'custom-not-found' }, [
-          h('div', { class: 'custom-not-found__video-wrap' }, [
-            h('video', {
-              class: 'custom-not-found__video',
-              src: videoSrc,
-              controls: true,
-              autoplay: true,
-              muted: true,
-              loop: true,
-              playsinline: true
-            })
-          ]),
-          h('p', { class: 'code' }, '404'),
-          h('h1', { class: 'title' }, 'Page not found!'),
-          h('div', { class: 'divider' }),
-          h(
-            'blockquote',
-            { class: 'quote' },
-            "But if you don't change your direction, and if you keep looking, you may end up where you are heading."
-          ),
-          h('div', { class: 'action' }, [
-            h(
-              'a',
-              {
-                class: 'link',
-                href: '/',
-                'aria-label': 'go to home'
-              },
-              'Take me home'
-            )
-          ])
-        ])
+      // https://vitepress.dev/guide/extending-default-theme#layout-slots
     })
   },
   enhanceApp({ app, router, siteData }) {
